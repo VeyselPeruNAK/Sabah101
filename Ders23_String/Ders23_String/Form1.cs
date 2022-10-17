0aa16fb07a08a1ed12249e85f9bbe62c0fc574af
Veysel Perü <veyselperunak@gmail.com>
2022-10-17 23:05:24 +0300

    Ders23_string

---
+++ Ders23_String/Ders23_String/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders23_String
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string testString1 = textBox1.Text;
+            string testString2 = textBox2.Text;
+            int result = string.Compare(testString1, testString2, true);
+            label1.Text = "";
+
+            if (result == 0)
+            {
+                label1.Text += "Two strings are equal";
+            }
+            else if (result == 1)
+            {
+                label1.Text += "Test String1 is greater than Test String2";
+            }
+            else if (result == -1)
+            {
+                label1.Text += "Test String1 is less than Test String2";
+            }
+        }
+    }
+}
